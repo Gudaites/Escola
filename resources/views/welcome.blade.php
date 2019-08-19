2eda2ba8bc60816ce07d64c130086cfb6c13cbcc
Henrique Gudaites <hgudaites@gmail.com>
2019-08-19 08:59:47 -0300

    Adicionado turmasid

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
                     <a href="{{ route('site.alunos') }}" class="list-group-item list-group-item-action">Alunos</a>
                     <a href="{{ route('site.alunos.adicionar') }}" class="list-group-item list-group-item-action">Cadastrar Alunos</a>
                     <a href="{{ route('site.turmas') }}" class="list-group-item list-group-item-action">Turmas</a>
-                    <a href="#" class="list-group-item list-group-item-action disabled">Cadastrar Turmas</a>
+                    <a href="{{ route('site.turmas.adicionar')}}" class="list-group-item list-group-item-action">Cadastrar Turmas</a>
                     <br>
                   </div>
     </div>
